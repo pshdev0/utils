e49dfac2bfa86f62f48d1ea2504271a48f53089c
pshdev0 <23234059+pshdev0@users.noreply.github.com>
2022-07-14 10:14:55 +0100

    Create auto-student-list.js

---
+++ coursera-auto-download/auto-student-list.js
@@ -0,0 +1,25 @@
+// this code forms part of one of the other larger auto files
+//
+// open your 
+//
+//
+//
+
+var START_INDEX = 0;
+var END_INDEX = 50;
+
+console.log("Scanning for students");
+var urls = document.getElementsByTagName('a');
+var rtn = {};
+for (url in urls) {
+    if(urls[url].href && urls[url].href.startsWith("https://www.coursera.org/teach/uol-graphics-programming/PjESn54jEey9ig43bzvlxw/grading/assignment-grading/Cg7EC/submission/")) {
+        rtn[urls[url].innerText] = urls[url].href;
+    }
+}
+let list2 = Object.entries(rtn);
+var output = "";
+for(var c1 = START_INDEX; c1 < Math.min(END_INDEX, list2.length); c1++) {
+    output += [list2[c1][0], list2[c1][1]].join("\n") + "\n";
+}
+copy(output);
+console.log("The data was copied to the clipboard.");
